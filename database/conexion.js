f13a2ef1fc5e481f9a6cd8958880bfce2e0c84bf
Karla <karla.a_21@hotmail.com>
2023-03-15 18:41:16 -0300

    version con nueva db

--- database/conexion.js
+++ database/conexion.js
@@ -1,8 +1,7 @@
 const {Sequelize} = require("sequelize");
 
-const sequelize = new Sequelize('game_score', 'postgres', 'postgresql', {
-    host: 'localhost',
-    dialect: 'postgres'
-});
+const sequelize = new Sequelize(
+    "postgres://dbscoregame_user:LV0tu7KjAVuYYeRCLWr9XNjhbQFxGYEW@dpg-cg93h8ik728n6517qgq0-a/dbscoregame"
+);
 
 module.exports = {sequelize};
