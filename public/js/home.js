1a194ef0bbdf4d53d6a908aa8af3680860267075
Karla <karla.a_21@hotmail.com>
2023-03-29 23:08:10 -0300

    Versión con perfil

--- public/js/home.js
+++ public/js/home.js
@@ -119,7 +119,7 @@ $("#previewPage").event("click", async (e)=> {  //Evento para regresar de pagina
 $(".logout").event("click", async (e)=> {
     localStorage.removeItem("gamescore-jwt");   //Eliminar token existente
     $(".login")[0].classList.remove("hidden");
-    $(".logout")[0].classList.add("hidden");
+    $("#menuUser")[0].classList.add("hidden");
     alert("Sesión cerrada con éxito");
     location.reload();
 });
@@ -146,7 +146,7 @@ $(".profile").event("click", async (e)=>{   //Abrir pagina de perfil
 (()=> { //Funcion autoejecutable para verificar token
     if (localStorage.getItem("gamescore-jwt")) {
         $(".login")[0].classList.add("hidden")
-        $(".logout")[0].classList.remove("hidden")   
+        $("#menuUser")[0].classList.remove("hidden")   
     }
 })();
 
