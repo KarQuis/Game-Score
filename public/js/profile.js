9ec88bd9ae0037b1ecfa499dfceb8fbeff81f4a4
Karla <karla.a_21@hotmail.com>
2023-03-29 21:54:23 -0300

    Versión con perfil

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,3 +1,52 @@
 import {$} from "../js/library.js";
 
+const reviewUser = $("#reviewsUser")[0];
 
+const getReviews = async (token, id)=> {   //verificar datos
+    try {
+        const response = await fetch('/review/profile/verify',
+        {
+          method:'GET',
+          headers: {'Content-Type': 'application/json', token: token, id: id},
+        })
+        return response.json();
+    } catch (error) {
+        console.log(`Error: ${err}`)
+    }
+};
+
+const goGame =  (id)=>{
+    window.open(`/game/${id}`, '_self');
+}
+
+const reloadReviews = async (review)=> {    //Funcion para mostrar reviews
+    // gamesCards.innerHTML = "";
+    review.forEach(element => {
+        let newArticle = document.createElement("article"); //Crear articulo
+        newArticle.setAttribute("data-info", `${element.idGame}`)   //integrar atributos a la div
+        newArticle.setAttribute("class", "cardReview bg-[#F8F4EA] shadow-lg rounded-lg my-4 p-4")
+        let newP1 = document.createElement("p");    //Crear parrafo
+        newP1.setAttribute("class", "text-xl font-semibold py-4");
+        newP1.innerText = `${element.score}`;
+        newArticle.appendChild(newP1);  //Insertar elementos en div
+        let newP2 = document.createElement("p");    //Crear parrafo
+        newP2.setAttribute("class", "pt-2");
+        newP2.innerText = `${element.content}`;
+        newArticle.appendChild(newP2);  //Insertar elementos en div
+
+        reviewUser.appendChild(newArticle); //Agregar review a nueva div
+    });
+    $(".cardReview").event("click", (e)=>{    //Evento para redirigir info de juego
+        const idGame = e.currentTarget.dataset.info ;  //currentTarget: traer elemento padre HTML seleccionado; dataset:extraer un campo 'data-' de HTML
+        goGame(idGame);
+    });
+};
+
+$("#seeReview").event("click", async ()=>{  //Evento para ver reviews
+    const reviews = await getReviews(localStorage.getItem("gamescore-jwt"), localStorage.getItem("gamescore-userId"));
+    if (reviews.code) {
+        alert(reviews.message)
+    } else {
+        reloadReviews(reviews)
+    }
+});
