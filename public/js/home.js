6bd71f6cfbfca11a43807f77220001246802c201
Karla <karla.a_21@hotmail.com>
2023-03-31 19:42:32 -0300

    Version con menu usuario

--- public/js/home.js
+++ public/js/home.js
@@ -149,7 +149,6 @@ $(".menuBtn").event("click", async (e)=>{
 });
 
 (()=> { //Funcion autoejecutable para verificar token
-    $("#menuUser")[0].classList.add("hidden");
     if (localStorage.getItem("gamescore-jwt")) {
         $(".login")[0].classList.add("hidden")
         $(".menuBtn")[0].classList.remove("hidden"); 
